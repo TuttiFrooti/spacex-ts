16c7a9b4df72897440a45bd04ab492e8fbb6a9fe
Logiz <l.samemami@gmail.com>
2021-02-21 20:12:00 +0100

    "Fixed" test

--- __tests__/truncateName.test.js
+++ __tests__/truncateName.test.js
@@ -1,5 +1,5 @@
 import { truncateMissionName } from '../src/script/utils/truncateName';
 
 test('Truncate mission name to fit cleanly', () => {
-  expect(truncateMissionName("Apollo Mission 11 Moon Landing")).toMatch('Apollo 12');
+  expect(truncateMissionName("Apollo Mission 11 Moon Landing")).toMatch('Apollo');
 });
